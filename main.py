6be387d734e82f66c1767bcc4ffd24bd5e7a5482
Success <successabalaka2002@gmail.com>
2023-06-13 21:00:01 +0100

    PostgreSQL

--- main.py
+++ main.py
@@ -79,8 +79,8 @@ class Comment(db.Model):
     text = db.Column(db.Text, nullable=False)
 
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 coming_from_code = False
 
